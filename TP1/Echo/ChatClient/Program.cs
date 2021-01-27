1f1eb6cc0aa59e3b4aa7e0a5ad3adb31a3e7ad78
Benjamin Vella <b3nj4m1n.vella@gmail.com>
2021-01-27 11:34:59 +0100

    Add Echo server and client

---
+++ TP1/Echo/ChatClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+
+namespace Echo
+{
+    class EchoClient
+    {
+        static TcpClient clientSocket;
+        public static void Write()
+        {
+            BinaryWriter writer = new BinaryWriter(clientSocket.GetStream());
+
+            while (true)
+            {
+
+                string str = Console.ReadLine();
+                writer.Write(str);
+            }
+        }
+        public static void Read()
+        {
+            BinaryReader reader = new BinaryReader(clientSocket.GetStream());
+
+            while (true)
+            {
+                string str = "response: ";
+                str = str + reader.ReadString();
+                Console.WriteLine(str);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            clientSocket = new TcpClient("localhost", 5000);
+            Thread ctThreadWrite = new Thread(Write);
+            Thread ctThreadRead = new Thread(Read);
+            ctThreadRead.Start();
+            ctThreadWrite.Start();
+        }
+    }
+}
