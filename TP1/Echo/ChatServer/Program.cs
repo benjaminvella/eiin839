045fff0430d941bcb0b1189cc99afb7541263f83
Benjamin Vella <b3nj4m1n.vella@gmail.com>
2021-01-27 11:34:59 +0100

    Add Echo server and client

---
+++ TP1/Echo/ChatServer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Echo
+{
+    class EchoServer
+    {
+        [Obsolete]
+        static void Main(string[] args)
+        {
+
+            Console.CancelKeyPress += delegate
+            {
+                System.Environment.Exit(0);
+            };
+
+            TcpListener ServerSocket = new TcpListener(5000);
+            ServerSocket.Start();
+
+            Console.WriteLine("Server started.");
+            while (true)
+            {
+                TcpClient clientSocket = ServerSocket.AcceptTcpClient();
+                handleClient client = new handleClient();
+                client.startClient(clientSocket);
+            }
+
+
+        }
+    }
+
+    public class handleClient
+    {
+        TcpClient clientSocket;
+        public void startClient(TcpClient inClientSocket)
+        {
+            this.clientSocket = inClientSocket;
+            Thread ctThread = new Thread(Echo);
+            ctThread.Start();
+        }
+
+
+
+        private void Echo()
+        {
+            NetworkStream stream = clientSocket.GetStream();
+            BinaryReader reader = new BinaryReader(stream);
+            BinaryWriter writer = new BinaryWriter(stream);
+
+            while (true)
+            {
+
+                string str = reader.ReadString();
+                Console.WriteLine(str);
+                writer.Write(str);
+            }
+        }
+
+
+
+    }
+
+}
