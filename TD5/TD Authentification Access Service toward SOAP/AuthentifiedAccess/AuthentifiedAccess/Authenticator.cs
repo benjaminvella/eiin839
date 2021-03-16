203da8c78bca29c969b693f584a0220f154c7f8f
tigli <tigli@unice.fr>
2021-03-16 19:07:36 +0100

    TD5

---
+++ TD5/TD Authentification Access Service toward SOAP/AuthentifiedAccess/AuthentifiedAccess/Authenticator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AuthentifiedAccess
+{
+    class Authenticator
+    {
+        private Dictionary<string, string> Credentials = new Dictionary<string, string>();
+        public Authenticator()
+        {
+            string[] logins = File.ReadAllLines(@"..\..\..\passwd.csv");
+            foreach (string login in logins)
+            {
+                string[] parts = login.Split(';');
+                Credentials.Add(parts[0].Trim(), parts[1].Trim());
+            }
+        }
+        public bool ValidateCredentials(string username, string password)
+        {
+            return Credentials.Any(entry => entry.Key == username && entry.Value == password);
+        }
+    }
+}
---
+++ TD5/TD Authentification Access Service toward SOAP/AuthentifiedAccess/AuthentifiedAccess/ServiceAccess.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Threading;
+
+namespace AuthentifiedAccess
+{
+    class ServiceAccess
+    {
+        /* Openweather access */
+
+        public void Weather(string city)
+        { 
+        // you can use the following keys to acces the web service
+                String key1 = "327f93f0117f1f0009c3a14ee69ff283";
+
+                                // url to request the weather in Avignon
+                string url1 = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",fr&appid=" + key1;
+
+                // create a web  Client
+                // A C#/.Net WebClient provides common methods for sending data to and receiving 
+                // data from a resource identified by a URL
+                WebClient client = new WebClient();
+                // download the data
+                string data = client.DownloadString(url1);
+
+                Console.WriteLine("connecting openweathermap.org");
+                // wait a bit for the server to have the time to send its answer
+                Thread.Sleep(300);
+
+                // show information of the data (json) that have been sent
+                JObject jso = JObject.Parse(data);
+                Console.WriteLine("in "+city+" the visibility is " + jso.SelectToken("visibility")+
+                    " meters and the weather is " + jso.SelectToken("weather[0].main"));           
+
+                // the end
+                Console.WriteLine("press a key to exit");
+
+                Console.ReadKey();
+        }
+    }
+}
