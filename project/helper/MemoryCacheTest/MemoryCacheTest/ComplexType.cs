15547597fc0f40562fc6da6ccdb220773e891aa2
Benjamin Vella <b3nj4m1n.vella@gmail.com>
2021-03-31 14:49:10 +0200

    Add cache

---
+++ project/helper/MemoryCacheTest/MemoryCacheTest/ComplexType.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryCacheTest
+{
+    class ComplexType
+    {
+        int _counter;
+
+        public ComplexType(int val_init)
+        {
+            counter = val_init;
+        }
+        public int counter
+            {
+            get 
+            { 
+                return _counter;
+            }
+            set
+            {
+                _counter = value;
+            }
+        }
+
+        public void incr()
+        {
+            counter++;
+        }
+        public string display()
+        {
+            return Convert.ToString(counter);
+        }
+    }
+}
