a071e6bb0e17c702e21e184ec818e357ced6db0b
Lidaou Denis ASSOTI <58354924+denisassoti@users.noreply.github.com>
2022-02-11 09:55:21 +0100

    basic server TD1

--- TD1/Echo/ChatServer/Program.cs
+++ TD1/Echo/ChatServer/Program.cs
@@ -48,21 +48,44 @@ namespace Echo
 
         private void Echo()
         {
+
             NetworkStream stream = clientSocket.GetStream();
             BinaryReader reader = new BinaryReader(stream);
             BinaryWriter writer = new BinaryWriter(stream);
+            string HTTP_ROOT = "../../../www/pub/";
 
             while (true)
             {
-
                 string str = reader.ReadString();
-                Console.WriteLine(str);
-                writer.Write(str);
+                bool isFile = false;
+                string method = str.Split(' ')[0];
+                string URI = str.Split(' ')[1];
+                if(method == "GET")
+                {
+                    if (Directory.Exists(HTTP_ROOT))
+                    {
+                        foreach (string file in Directory.GetFiles(HTTP_ROOT, "*"))
+                        {
+                            if (HTTP_ROOT + URI == file)
+                            {
+                                Console.WriteLine(file);
+                                string fileContent = "HTTP/1.0 200 OK\n\n";
+                                fileContent += File.ReadAllText(file);
+                                Console.WriteLine(fileContent);
+                                writer.Write(fileContent);
+                                isFile = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!isFile)
+                    {
+                        Console.WriteLine(str);
+                        writer.Write(str);
+                    }
+                }
+                
             }
         }
-
-
-
     }
-
 }
